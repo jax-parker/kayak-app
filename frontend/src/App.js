390d4ab747647340bc4fa754f683bed4c5e46092
jax-parker <parkyjax711@gmail.com>
2023-10-22 14:33:19 +0000

    add sign up form and css for form and buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import SignUpForm from './pages/auth/SighUpForm';
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" render={() => <h1>Home Page</h1>}/>
         <Route exact path="/signin" render={() => <h1>Sign In</h1>}/>
-        <Route exact path="/signup" render={() => <h1>Sign Up</h1>}/>
+        <Route exact path="/signup" render={() => <SignUpForm />}/>
         <Route render={() => <p>Page not found</p>}/>
       </Switch>
      </Container>
