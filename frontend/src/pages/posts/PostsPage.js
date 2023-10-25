2c76a5d86b3b1cf8d8869793f364deee5e92a41b
jax-parker <parkyjax711@gmail.com>
2023-10-25 13:44:46 +0000

    add search functionality

--- frontend/src/pages/posts/PostsPage.js
+++ frontend/src/pages/posts/PostsPage.js
@@ -12,16 +12,19 @@ import styles from "../../styles/PostsPage.module.css";
 import { useLocation } from "react-router";
 import { axiosReq } from '../../api/axiosDefaults';
 import NoResults from "../../assets/no-results.png";
+import { FormControl } from "react-bootstrap";
 
 function PostsPage({ message, filter = ""}) {
   const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     const fetchPosts= async () => {
       try {
-        const {data} = await axiosReq.get(`/posts/?${filter}`);
+        const {data} = await axiosReq.get(`/posts/?${filter}search=${query}`);
         setPosts({...posts, results: [...data]});
         setHasLoaded(true);
       } catch(err){
@@ -36,12 +39,22 @@ function PostsPage({ message, filter = ""}) {
     return () => {
       clearTimeout(timer);
     };
-  }, [filter, pathname])
+  }, [filter, query, pathname])
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+        <i className={`fas fa-search${styles.Searchicon}`} />
+        <Form className={styles.SearchBar}
+        onSubmit={(event) => event.preventDefault()}>
+          <FormControl
+          value= {query}
+          onChange={(event => setQuery(event.target.value))}
+          type="text"
+          className="mr-sm-2"
+          placeholder="Search posts"/>
+        </Form>
 
         {
           hasLoaded 
