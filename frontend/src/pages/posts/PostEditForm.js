b912569225ebe9751c5bb282404e80cc282c5a0b
jax-parker <parkyjax711@gmail.com>
2023-10-26 12:12:59 +0000

    add edit, delete function

---
+++ frontend/src/pages/posts/PostEditForm.js
@@ -0,0 +1,206 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+import { useHistory, useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+
+function PostEditForm() {
+  const [errors, setErrors] = useState({});
+
+  const [postData, setPostData] = useState({
+    launchsite_name: "",
+    location: "",
+    what3words: "",
+    comments: "",
+    image: "",
+  });
+  const { launchsite_name, location, what3words, comments, image } = postData;
+
+  const imageInput = useRef(null);
+  const history = useHistory();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/${id}/`);
+        const { launchsite_name, location, what3words, comments, image, is_owner } = data;
+
+        is_owner ? setPostData({ launchsite_name, location, what3words, comments, image }) : history.push("/");
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [history, id]);
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const formData = new FormData();
+
+    formData.append('launchsite_name', launchsite_name)
+    formData.append('location', location)
+    formData.append('what3words', what3words)
+    formData.append('comments', comments)
+    if (imageInput?.current?.files[0]) {
+        formData.append("image", imageInput.current.files[0]);
+      }
+  
+      try {
+        await axiosReq.put(`/posts/${id}/`, formData);
+        history.push(`/posts/${id}`);
+      } catch (err) {
+        console.log(err);
+        if (err.response?.status !== 401) {
+          setErrors(err.response?.data);
+        }
+      }
+    };
+
+  const textFields = (
+    <div className="text-center">
+      <Form.Group>
+        <Form.Label>Launch Site Name</Form.Label>
+        <Form.Control
+          type="text"
+          name="launchsite_name"
+          value={launchsite_name}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.launchsite_name?.map((message, idx) =>(
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
+        <Form.Label>Location/Town</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={1}
+          name="location"
+          value={location}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.location?.map((message, idx) =>(
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
+        <Form.Label>What3words</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={1}
+          name="what3words"
+          value={what3words}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.what3words?.map((message, idx) =>(
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
+        <Form.Label>Comments</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          name="comments"
+          value={comments}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.comments?.map((message, idx) =>(
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Bright}`}
+        onClick={() => history.goBack()}
+      >
+        cancel
+      </Button>
+      <Button className={`${btnStyles.Button} ${btnStyles.Bright}`} type="submit">
+        save
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Bright} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                
+
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+                ref={imageInput}
+              />
+            </Form.Group>
+            {errors?.image?.map((message, idx) =>(
+              <Alert variant="warning" key={idx}>
+              {message}
+              </Alert>
+            ))}
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default PostEditForm;
--- frontend/src/pages/posts/PostsPage.js
+++ frontend/src/pages/posts/PostsPage.js
@@ -4,19 +4,20 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-import Post from './Post';
+
+import Post from "./Post";
 import Asset from "../../components/Asset";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
 import { useLocation } from "react-router";
-import { axiosReq } from '../../api/axiosDefaults';
+import { axiosReq } from "../../api/axiosDefaults";
+
 import NoResults from "../../assets/no-results.png";
-import { FormControl } from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 
-function PostsPage({ message, filter = ""}) {
+function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
@@ -24,15 +25,16 @@ function PostsPage({ message, filter = ""}) {
   const [query, setQuery] = useState("");
 
   useEffect(() => {
-    const fetchPosts= async () => {
+    const fetchPosts = async () => {
       try {
-        const {data} = await axiosReq.get(`/posts/?${filter}search=${query}`);
-        setPosts({...posts, results: [...data]});
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+        setPosts(data);
         setHasLoaded(true);
-      } catch(err){
+      } catch (err) {
         console.log(err);
       }
-    }
+    };
+
     setHasLoaded(false);
     const timer = setTimeout(() => {
       fetchPosts();
@@ -41,30 +43,30 @@ function PostsPage({ message, filter = ""}) {
     return () => {
       clearTimeout(timer);
     };
-  }, [filter, query, pathname])
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
         <i className={`fas fa-search ${styles.SearchIcon}`} />
-        <Form className={styles.SearchBar}
-        onSubmit={(event) => event.preventDefault()}>
-          <FormControl
-          value= {query}
-          onChange={(event => setQuery(event.target.value))}
-          type="text"
-          className="mr-sm-2"
-          placeholder="Search posts"/>
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search posts"
+          />
         </Form>
 
-        {
-          hasLoaded 
-            ? (
-            <>
-              {
-                posts.results.length ? (
-                  <InfiniteScroll
+        {hasLoaded ? (
+          <>
+            {posts.results?.length ? (
+              <InfiniteScroll
                 children={posts.results.map((post) => (
                   <Post key={post.id} {...post} setPosts={setPosts} />
                 ))}
@@ -73,21 +75,17 @@ function PostsPage({ message, filter = ""}) {
                 hasMore={!!posts.next}
                 next={() => fetchMoreData(posts, setPosts)}
               />
-                    
-                  ) 
-                  : (
-                    <Container className={appStyles.Content}>
-                      <Asset src={NoResults} message={message} />
-
-                    </Container>
-                  )}
-            </>
             ) : (
               <Container className={appStyles.Content}>
-                <Asset spinner />
+                <Asset src={NoResults} message={message} />
               </Container>
-            )
-        }
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
