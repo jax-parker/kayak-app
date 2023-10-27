6dff2e15c9a93f639358d23e06f66ef231dadd71
jax-parker <parkyjax711@gmail.com>
2023-10-27 16:34:36 +0000

    add wireframes to readme

--- frontend/src/pages/posts/Post.js
+++ frontend/src/pages/posts/Post.js
@@ -76,7 +76,7 @@ const Post = (props) => {
   };
 
   return (
-    <Card className={styles.Post}>
+    <Card className={styles.Contacts}>
       <Card.Body>
         <Media className="align-items-center justify-content-between">
           <Link to={`/profiles/${profile_id}`}>
