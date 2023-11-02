3f3b0a795cc258981145e573d533700e682ecabf
jax-parker <parkyjax711@gmail.com>
2023-11-02 12:07:58 +0000

    add form fields to sales

--- frontend/src/pages/sales/SalesCreateForm.js
+++ frontend/src/pages/sales/SalesCreateForm.js
@@ -5,7 +5,7 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-
+import Asset from "../../components/Asset";
 import Upload from "../../assets/upload.png";
 
 import styles from "../../styles/SalesCreateEditForm.module.css";
@@ -16,12 +16,76 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 function SalesCreateForm() {
     const [errors, setErrors] = useState({});
 
+    const [salesData, setSalesData] = useState({
+        title:"",
+        description: "",
+        condition:"",
+        price:"",
+        location:"",
+        image:"",
+    })
+    const { title, description, condition, price, location, image } = salesData;
+
+    // const conditionChoices = [
+    //     { value: "new", label: "New" },
+    //     { value: "nearly_new", label: "Nearly New" },
+    //     { value: "used", label: "Used" },
+    //   ];
+
+    const handleChange = (event) => {
+        setSalesData({
+            ...salesData,
+            [event.target.name]: event.target.value,
+        });
+    }
+
 
     const textFields = (
       <div className="text-center">
-        {/* Add your form fields here */}
-  
-      
+        <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          name="description"
+          value={description}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {/* <Form.Group>
+        <Form.Label>Condition</Form.Label>
+        <Form.Select name="condition" options={conditionChoices} />
+      </Form.Group> */}
+      <Form.Group>
+        <Form.Label>Price £</Form.Label>
+        <Form.Control
+          type="text"
+          rows={1}
+          name="price"
+          value={price}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Location</Form.Label>
+        <Form.Control
+          type="text"
+          rows={2}
+          name="location"
+          value={location}
+          onChange={handleChange}
+        />
+      </Form.Group>
+        
       
         <Button
           className={`${btnStyles.Button} ${btnStyles.Blue}`}
@@ -48,7 +112,10 @@ function SalesCreateForm() {
                     className="d-flex justify-content-center"
                     htmlFor="image-upload"
                   >
-                    ASSET
+                    <Asset
+                    src={Upload}
+                    message="Click or tap to upload an image"
+                  />
                   </Form.Label>
   
               </Form.Group>
