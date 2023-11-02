2d8e442d5e4698a8f815f9b6feb3fcecb442c65b
jax-parker <parkyjax711@gmail.com>
2023-11-02 12:52:41 +0000

    create functions for sales

--- frontend/src/pages/sales/SalesCreateForm.js
+++ frontend/src/pages/sales/SalesCreateForm.js
@@ -1,48 +1,78 @@
-import React, { useState } from "react";
-
+import React, { useRef, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
 import Asset from "../../components/Asset";
 import Upload from "../../assets/upload.png";
-
 import styles from "../../styles/SalesCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function SalesCreateForm() {
-    const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({});
 
-    const [salesData, setSalesData] = useState({
-        title:"",
-        description: "",
-        condition:"",
-        price:"",
-        location:"",
-        image:"",
-    })
-    const { title, description, condition, price, location, image } = salesData;
+  const [salesData, setSalesData] = useState({
+    title: "",
+    description: "",
+    condition: "",
+    price: "",
+    location: "",
+    image: "",
+  });
+  const { title, description, condition, price, location, image } = salesData;
 
-    // const conditionChoices = [
-    //     { value: "new", label: "New" },
-    //     { value: "nearly_new", label: "Nearly New" },
-    //     { value: "used", label: "Used" },
-    //   ];
+  const imageInput = useRef(null);
+  const history = useHistory();
 
-    const handleChange = (event) => {
-        setSalesData({
-            ...salesData,
-            [event.target.name]: event.target.value,
-        });
+  const handleChange = (event) => {
+    setSalesData({
+      ...salesData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setSalesData({
+        ...salesData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
     }
+  };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const formData = new FormData();
 
-    const textFields = (
-      <div className="text-center">
-        <Form.Group>
+    formData.append('title', title)
+    formData.append('description', description)
+    formData.append('condition', condition)
+    formData.append('price', price)
+    formData.append('location', location)
+    formData.append('image', imageInput.current.files[0])
+
+    try {
+      const {data} = await axiosReq.post('/sales/', formData);
+      history.push(`/sales/${data.id}`)
+    } catch(err){
+      if (err.response?.status !==401) {
+        setErrors(err.response?.data)
+      }
+
+    }
+  }
+
+  const textFields = (
+    <div className="text-center">
+      <Form.Group>
         <Form.Label>Title</Form.Label>
         <Form.Control
           type="text"
@@ -51,6 +81,11 @@ function SalesCreateForm() {
           onChange={handleChange}
         />
       </Form.Group>
+      {errors?.launchsite_name?.map((message, idx) =>(
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
       <Form.Group>
         <Form.Label>Description</Form.Label>
         <Form.Control
@@ -61,73 +96,126 @@ function SalesCreateForm() {
           onChange={handleChange}
         />
       </Form.Group>
-      {/* <Form.Group>
+      {errors?.location?.map((message, idx) =>(
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
         <Form.Label>Condition</Form.Label>
-        <Form.Select name="condition" options={conditionChoices} />
-      </Form.Group> */}
+        <Form.Control
+          type="text"
+          rows={1}
+          name="condition"
+          value={condition}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.what3words?.map((message, idx) =>(
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
       <Form.Group>
         <Form.Label>Price £</Form.Label>
         <Form.Control
-          type="text"
+          as="textarea"
           rows={1}
           name="price"
           value={price}
           onChange={handleChange}
         />
       </Form.Group>
+      {errors?.comments?.map((message, idx) =>(
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+        
+      ))}
       <Form.Group>
         <Form.Label>Location</Form.Label>
         <Form.Control
           type="text"
-          rows={2}
+          rows={1}
           name="location"
           value={location}
           onChange={handleChange}
         />
       </Form.Group>
+      {errors?.comments?.map((message, idx) =>(
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
         
-      
-        <Button
-          className={`${btnStyles.Button} ${btnStyles.Blue}`}
-          onClick={() => {}}
-        >
-          cancel
-        </Button>
-        <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-          create
-        </Button>
-      </div>
-    );
-  
-    return (
-      <Form>
-        <Row>
-          <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
-            <Container
-              className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
-            >
-              <Form.Group className="text-center">
-                
-                  <Form.Label
-                    className="d-flex justify-content-center"
-                    htmlFor="image-upload"
-                  >
-                    <Asset
+      ))}
+
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Bright}`}
+        onClick={() => history.goBack()}
+      >
+        cancel
+      </Button>
+      <Button className={`${btnStyles.Button} ${btnStyles.Bright}`} type="submit">
+        create
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+              {image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Bright} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label
+                  className="d-flex justify-content-center"
+                  htmlFor="image-upload"
+                >
+                  <Asset
                     src={Upload}
                     message="Click or tap to upload an image"
                   />
-                  </Form.Label>
-  
-              </Form.Group>
-              <div className="d-md-none">{textFields}</div>
-            </Container>
-          </Col>
-          <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
-            <Container className={appStyles.Content}>{textFields}</Container>
-          </Col>
-        </Row>
-      </Form>
-    );
-  }
+                </Form.Label>
+              )}
+
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+                ref={imageInput}
+              />
+            </Form.Group>
+            {errors?.image?.map((message, idx) =>(
+              <Alert variant="warning" key={idx}>
+              {message}
+              </Alert>
+            ))}
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container className={styles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
 
 export default SalesCreateForm;
